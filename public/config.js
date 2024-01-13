d32f65a90c052695931bab965a919b8c63657818
Jayphen Simpson <jayphen.simpson@n26.com>
2024-01-13 17:37:54 +0100

    Make all fields configurable by env

--- public/config.js
+++ public/config.js
@@ -1,10 +1,6 @@
 /*----- Player name -----*/
 window.playername = "IPTVEditor Web Player";
 
-/*----- DNS -----*/
-//Iptv provider dns url (for example "http://domain.com:80")
-window.dns = "http://domain.com:80";
-
 /*----- CORS -----*/
 /*/ False if iptv provider has the Access-Control-Allow-Origin set on "*" or allows your player domain.
 It will work with most of the providers.
@@ -16,16 +12,12 @@ window.cors = false; //true
 
 /*---- HTTPS -----*/
 /* If streams are using an ssl protol change this box on true.*/
-window.https = false;  //true;
+window.https = false; //true;
 
 /*----- TMDB API [OPTIONAL] -----*/
 /* By default player will use movie info from the provider. In case these info are missing it will be used tmdb as alternative
 Here you can get a tmdb api key: https://developers.themoviedb.org/3/getting-started/introduction  */
 window.tmdb = "";
 
-
-
-
-
 //!!!!!!! Don't change this !!!!!!!
-document.getElementsByTagName("title")[0].innerText = window.playername
+document.getElementsByTagName("title")[0].innerText = window.playername;
--- public/config.php
+++ public/config.php
@@ -1,11 +1,13 @@
 <?php
-$db_url = "localhost";
-$db_name = ""; //database name
-$db_username = ""; //database account username
-$db_password=""; //database account password
-$epg_url = ""; //epg xml url (http://domain.com:80/xmltv.php?username=pippo&password=test)
+$db_url = getenv('DB_HOST') ?: "localhost";
+$db_name = getenv('MYSQL_DATABASE') ?: "db"; //database name
+$db_username = getenv('MYSQL_USER') ?: "myuser"; //database account username
+$db_password = getenv('MYSQL_PASSWORD') ?: "mypassword"; //database account password
+$epg_url_base = getenv('EPG_URL');
+$epg_username = getenv('EPG_USERNAME');
+$epg_password = getenv('EPG_PASSWORD');
+$epg_url = $epg_url_base . "?username=" . $epg_username . "&password=" . $epg_password;
 $epg_valid_hours = "+12 hours"; //if database has epg available for less than 12 hours it will be updated. Alternative values: "+1 day", "+2 days", "+1 week" etc etc
 $cors = false;
 //To avoid wasting server resources, epg update will be triggered when an user login and database has less than 12 hours of programmes.
-
 ?>
